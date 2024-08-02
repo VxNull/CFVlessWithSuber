388015a3aac32e2695a4812a80e310fd454bdd9f
null <null@github>
2024-08-02 19:53:42 +0800

    🐞 fix:  base64 err, remove countryCodeEmoji in vless mode

--- _worker.js
+++ _worker.js
@@ -160,7 +160,7 @@ async function handleClSub(goodIps, clash_template_url, userID, request, reqSpee
 
 	//proxyGroupCountryList生成proxy_groups_country_name_list
 	let proxy_groups_country_name_list = proxyGroupCountryList.map(proxyGroup => {
-		return `- ${countryCodeEmoji(proxyGroup.country)}${proxyGroup.country}\n`;
+		return `- ${countryCodeEmoji(proxyGroup.country)} ${proxyGroup.country}\n`;
 	});
 
 	//替换#      - {{proxy_groups_country_name_list}}
@@ -172,7 +172,7 @@ async function handleClSub(goodIps, clash_template_url, userID, request, reqSpee
 	// proxyGroupCountryList 生成 proxy_groups_list_by_country
 	let proxy_groups_list_by_country = [];
 	proxyGroupCountryList.forEach(proxyGroup => {
-		proxy_groups_list_by_country.push(`- name: ${countryCodeEmoji(proxyGroup.country)}${proxyGroup.country}\n`);
+		proxy_groups_list_by_country.push(`- name: ${countryCodeEmoji(proxyGroup.country)} ${proxyGroup.country}\n`);
 		proxy_groups_list_by_country.push(`  type: load-balance\n`);
 		proxy_groups_list_by_country.push(`  proxies:\n`);
 		let proxies = proxyGroup.proxies;
@@ -247,7 +247,7 @@ function generateProxiesList(goodIps, userID, hostname, reqSpeed, type) {
             }
 
             if (type === 'v') {
-                newProxiesList.push(`${proto}://${userID}@${thisProxyIp}:443?encryption=none&security=tls&type=ws&host=${hostname}&sni=${hostname}&fp=random&path=%2Fproxyip%3D${specProxyIp}%2F%3Fed%3D2176#${countryCodeEmoji(goodIP.country)}${goodIP.country}${i}\n`);
+                newProxiesList.push(`${proto}://${userID}@${thisProxyIp}:443?encryption=none&security=tls&type=ws&host=${hostname}&sni=${hostname}&fp=random&path=%2Fproxyip%3D${specProxyIp}%2F%3Fed%3D2176#${goodIP.country}${i}\n`);
             } else if (type === 'c') {
                 newProxiesList.push(`- {"name":"${goodIP.country}${i}","type":"${proto}","server":"${thisProxyIp}","port":443,"uuid":"${userID}","tls":true,"servername":"${hostname}","network":"ws","ws-opts":{"path":"/proxyip=${specProxyIp}?ed=2176","headers":{"host":"${hostname}"}}}\n`);
                 newProxiesNameList.push(`- ${goodIP.country}${i}\n`);
